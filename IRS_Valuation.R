f7bb809bc8476baef7c01f208882f97b227fa108
MarvinGauer <gauermar@student.hu-berlin.de>
2017-11-30 11:28:30 +0100

    Gross Outline of the Project

---
+++ IRS_Valuation.R
@@ -0,0 +1,15 @@
+############################################################################
+######## SFM1 - Project -> Compare the three different ways to value an IRS
+############################################################################
+
+######## 1. Input Parameters
+
+######## 2. Valuation
+
+######## 2.1. Valuation in Terms of Bond Prizes
+
+######## 2.2. Valuation in Terms of FRA Prizes
+
+######## 2.3. Valuation in Terms of XXX Prizes
+
+######## 3. Results & Graphics
